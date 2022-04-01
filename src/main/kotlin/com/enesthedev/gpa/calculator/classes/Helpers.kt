863f155ebfdccdaf15006bcc3017f72fb9cb59bb
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-01 14:43:56 +0100

    Add: getRealGrade method for calculateing one term Grade GPA
    Refactor: Reformat code with using IDE

--- src/main/kotlin/com/enesthedev/gpa/calculator/classes/Helpers.kt
+++ src/main/kotlin/com/enesthedev/gpa/calculator/classes/Helpers.kt
@@ -9,13 +9,21 @@ class Helpers {
         @JvmStatic
         // Bir string tipindeki verinin tamamının sayı olup olmadığını kontrol eder.
         fun isNumber(string: String): Boolean {
-            if (string == null || string.isEmpty()) {
-                return false;
+            if (string.isEmpty()) {
+                return false
             }
             if (!string.all { Character.isDigit(it) }) {
-                return false;
+                return false
             }
-            return true;
+            return true
+        }
+
+        // Sınav değerinin sınavın GANO üstündeki etkisiyle çarpımını döndürür.
+        fun getRealGrade(gradeValue: Int, gradeRatio: Double?): Double {
+            if (gradeRatio !== null) {
+                return gradeValue * (gradeRatio / 100)
+            }
+            return 0.0
         }
     }
 }
