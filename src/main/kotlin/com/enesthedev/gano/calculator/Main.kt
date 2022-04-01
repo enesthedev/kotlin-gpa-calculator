e08805201137cae864978f0ad0b0acb23da35533
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-01 04:06:50 +0100

    Add: Language system with english and turkish options
    Add: Dynamic language select from device

--- src/main/kotlin/com/enesthedev/gano/calculator/Main.kt
+++ src/main/kotlin/com/enesthedev/gano/calculator/Main.kt
@@ -1,7 +1,23 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.util.*
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+val languages = arrayOf("tr", "en")
+val keys = arrayOf(
+    mapOf(
+        "welcome" to "Ders Not Ortalaması hesaplayıcıya hoşgeldiniz"
+    ),
+    mapOf(
+        "welcome" to "en"
+    ),
+)
+
+fun main(args: Array<String>) {
+    /**
+     * Şuanki sistem dilini kullanaraktan aplikasyonda dil desteği verdiğim dillerin arasında olup olmadığını sorguladım ve
+     * map tipinde dil bilgilerini currentLanguage değişkenine döndürdüm. Yararlandığım kaynağın adresini altta bulabilirsiniz.
+     * https://discuss.kotlinlang.org/t/python-like-dictionaries-in-kotlin/2008
+     */
+    val systemLanguage = Locale.getDefault().language
+    val currentLanguage = if (languages.contains(systemLanguage)) keys[languages.indexOf(systemLanguage)] else keys[languages.indexOf("en")];
+
+    println(currentLanguage.get("welcome"))
 }
