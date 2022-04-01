12146b566f931d97538e3d76e758d6147f149608
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-01 13:48:19 +0100

    Add: isNumber helper function with comments

--- src/main/kotlin/com/enesthedev/gpa/calculator/classes/Helpers.kt
+++ src/main/kotlin/com/enesthedev/gpa/calculator/classes/Helpers.kt
@@ -1,4 +1,21 @@
 package com.enesthedev.gpa.calculator.classes
 
 class Helpers {
+    /**
+     * Yardımcı sınıf
+     * Bu sınıf programın ihtiyaçlarına göre tekrar kullanılabilir fonksiyonlar içerir.
+     */
+    companion object {
+        @JvmStatic
+        // Bir string tipindeki verinin tamamının sayı olup olmadığını kontrol eder.
+        fun isNumber(string: String): Boolean {
+            if (string == null || string.isEmpty()) {
+                return false;
+            }
+            if (!string.all { Character.isDigit(it) }) {
+                return false;
+            }
+            return true;
+        }
+    }
 }
