f5bafe8679878c7d5a19443ba631114a9c3e44b1
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-01 13:56:42 +0100

    Add: midtermRatio input with default value 20
    Refactor: Changed Language getters function to direct index getter

--- src/main/kotlin/com/enesthedev/gpa/calculator/Main.kt
+++ src/main/kotlin/com/enesthedev/gpa/calculator/Main.kt
@@ -1,18 +1,6 @@
-import com.enesthedev.gpa.calculator.constants.FALLBACK_LANGUAGE
+import com.enesthedev.gpa.calculator.classes.Helpers
+import com.enesthedev.gpa.calculator.classes.Language
 import java.util.*
-
-val languages = arrayOf("tr", "en")
-val keys = arrayOf(
-    mapOf(
-        "welcome" to "Ders Not Ortalaması hesaplayıcıya hoşgeldiniz",
-        "lesson" to "Lütfen aldığınız dersin ismini/kodunu giriniz:",
-        "midterm" to "Lütfen %s dersinden aldığınız vize notunu giriniz:",
-        "invalid" to "Girdiğiniz %s geçersiz."
-    ),
-    mapOf(
-        "welcome" to "en"
-    ),
-)
 
 fun main(args: Array<String>) {
     /**
@@ -21,34 +9,30 @@ fun main(args: Array<String>) {
      * https://discuss.kotlinlang.org/t/python-like-dictionaries-in-kotlin/2008
      */
     val systemLanguage = Locale.getDefault().language
-    val currentLanguage =
-        if (languages.contains(systemLanguage))
-            keys[languages.indexOf(systemLanguage)]
-        else
-            // FALLBACK_LANGUAGE değeri constants/Language.kt'den gelmekte.
-            // Bu tarz sabit değeler için yeni bir paket oluşturdum. Yararlandığım kaynağın adresini altta bulabilirsiniz.
-            // https://stackoverflow.com/questions/44038721/constants-in-kotlin-whats-a-recommended-way-to-create-them
-            keys[languages.indexOf(FALLBACK_LANGUAGE)];
+    val currentLanguage = Language.getKeyMapOfLanguage(systemLanguage)
 
-    println(currentLanguage.get("welcome"));
-    println(currentLanguage.get("lesson"));
+    println(currentLanguage["welcome"])
 
-    val lessonName: String? = readLine();
+    println(currentLanguage["lesson"])
+    val lessonName: String? = readLine()
+
     if (lessonName == null || lessonName.isEmpty()) {
-        println(currentLanguage.get("invalid")?.format("ders ismi"))
+        println(currentLanguage["invalid"]?.format("ders ismi"))
         return;
     }
 
-    println(currentLanguage.get("midterm")?.format(lessonName))
-    val midtermValue: String? = readLine();
-    if (midtermValue == null || midtermValue.isEmpty()) {
-        println(currentLanguage.get("invalid")?.format("not"))
+    println(currentLanguage["midterm"]?.format(lessonName))
+    val midtermValue: String? = readLine()
+
+    if (midtermValue?.let { Helpers.isNumber(it) } == false) {
+        println(currentLanguage["invalid"]?.format("not"))
         return;
     }
 
-    if (!midtermValue.all { Character.isDigit(it) }) {
-        println(currentLanguage.get("invalid")?.format("not"))
-        return;
-    }
+    println(currentLanguage["midterm.ratio"]?.format(lessonName))
+    var midtermRatio: String? = readLine()
 
+    if ((midtermRatio !== null && midtermRatio.isEmpty()) || midtermRatio?.let { Helpers.isNumber(it) } == false) {
+        midtermRatio = "20";
+    }
 }
