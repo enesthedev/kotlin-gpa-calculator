eb7e57691a09bb3c611f49e6273f2da01e7a43ed
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-01 04:13:30 +0100

    Refactor: Changed value of currentLanguage else 'en' to FALLBACK_LANGUAGE

--- src/main/kotlin/com/enesthedev/gano/calculator/Main.kt
+++ src/main/kotlin/com/enesthedev/gano/calculator/Main.kt
@@ -1,3 +1,4 @@
+import com.enesthedev.gano.calculator.constants.FALLBACK_LANGUAGE
 import java.util.*
 
 val languages = arrayOf("tr", "en")
@@ -17,7 +18,14 @@ fun main(args: Array<String>) {
      * https://discuss.kotlinlang.org/t/python-like-dictionaries-in-kotlin/2008
      */
     val systemLanguage = Locale.getDefault().language
-    val currentLanguage = if (languages.contains(systemLanguage)) keys[languages.indexOf(systemLanguage)] else keys[languages.indexOf("en")];
+    val currentLanguage =
+        if (languages.contains(systemLanguage))
+            keys[languages.indexOf(systemLanguage)]
+        else
+            // FALLBACK_LANGUAGE değeri constants/Language.kt'den gelmekte.
+            // Bu tarz sabit değeler için yeni bir paket oluşturdum. Yararlandığım kaynağın adresini altta bulabilirsiniz.
+            // https://stackoverflow.com/questions/44038721/constants-in-kotlin-whats-a-recommended-way-to-create-them
+            keys[languages.indexOf(FALLBACK_LANGUAGE)];
 
     println(currentLanguage.get("welcome"))
 }
