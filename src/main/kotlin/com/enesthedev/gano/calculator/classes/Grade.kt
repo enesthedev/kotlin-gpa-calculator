03d4fe046a55237f434ff514eed9b6d106e7b0f1
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-01 03:30:23 +0100

    Add: int value and double ratio props
    Add: result method for get real GPA Grade

--- src/main/kotlin/com/enesthedev/gano/calculator/classes/Grade.kt
+++ src/main/kotlin/com/enesthedev/gano/calculator/classes/Grade.kt
@@ -1,4 +1,25 @@
 package com.enesthedev.gano.calculator.classes
 
 class Grade {
+    var value: Int = 0
+    var ratio: Double = 0
+
+    /**
+     * Not sınıfı
+     * Bu sınıf value ve ratio isminde biri sayı diğeri ondalık sayı tipinde iki adet eleman içerir.
+     * Bunun sebebi GPA hesaplamızda vize notu ve final notunun oranlarının ve değerlerinin farklı olmasıdır.
+     *
+     * result metodu ise bize son ortalamada kullanabileceğimiz value ile ratio'nun çarpımını verir.
+     *
+     * Örnek: GPA'ya %20 etkisi olan bir vizeden 60 aldığımızı varsayıyorum bu metod sayesinde 60 * 0.2 = 12 çıktısını alacağız.
+     */
+
+    constructor(value: Int, ratio: Double) {
+        this.value = value;
+        this.ratio = ratio;
+    }
+
+    fun result(): Double {
+        return this.value * this.ratio;
+    }
 }
