3e847ed1268bdc01b4ea9970bd95a487bbc8f817
Enes Bayraktar <enes.bayraktar@outlook.com.tr>
2022-04-01 15:02:58 +0100

    Add: Result information parse
    Refactor: Reformat using IDE
    
    Add: getRealGrade Helper for calculate resultValue

--- src/main/kotlin/com/enesthedev/gpa/calculator/Main.kt
+++ src/main/kotlin/com/enesthedev/gpa/calculator/Main.kt
@@ -1,5 +1,7 @@
-import com.enesthedev.gpa.calculator.classes.*
-import com.enesthedev.gpa.calculator.constants.*
+import com.enesthedev.gpa.calculator.classes.Helpers
+import com.enesthedev.gpa.calculator.classes.Language
+import com.enesthedev.gpa.calculator.constants.FINAL_RATIO
+import com.enesthedev.gpa.calculator.constants.VISA_RATIO
 import java.util.*
 
 fun main(args: Array<String>) {
@@ -17,37 +19,60 @@ fun main(args: Array<String>) {
     val lessonName: String? = readLine()
 
     if (lessonName == null || lessonName.isEmpty()) {
-        println(currentLanguage["invalid"]?.format("ders ismi"))
-        return;
+        println(currentLanguage["invalid"]?.format(currentLanguage["lesson.name"]))
+        return
     }
 
-    println(currentLanguage["grade"]?.format(lessonName, currentLanguage["visa"]))
+    println(currentLanguage["grade"]?.format(
+        lessonName,
+        currentLanguage["visa"]
+    ))
     val midtermValue: String? = readLine()
 
     if (midtermValue?.let { Helpers.isNumber(it) } == false || midtermValue?.toInt()!! > 100) {
-        println(currentLanguage["invalid"]?.format("not"))
-        return;
+        println(currentLanguage["invalid"]?.format(currentLanguage["note"]))
+        return
     }
 
-    println(currentLanguage["ratio"]?.format(lessonName, currentLanguage["visa"], VISA_RATIO))
+    println(currentLanguage["ratio"]?.format(
+        lessonName,
+        currentLanguage["visa"],
+        VISA_RATIO
+    ))
     var midtermRatio: String? = readLine()
 
     if ((midtermRatio !== null && midtermRatio.isEmpty()) || midtermRatio?.let { Helpers.isNumber(it) } == false) {
-        midtermRatio = VISA_RATIO.toString();
+        midtermRatio = VISA_RATIO.toString()
     }
 
-    println(currentLanguage["grade"]?.format(lessonName, currentLanguage["final"]))
+    println(currentLanguage["grade"]?.format(
+        lessonName,
+        currentLanguage["final"]
+    ))
     val finalValue: String? = readLine()
 
     if (finalValue?.let { Helpers.isNumber(it) } == false || finalValue?.toInt()!! > 100) {
         println(currentLanguage["invalid"]?.format("not"))
-        return;
+        return
     }
 
-    println(currentLanguage["ratio"]?.format(lessonName, currentLanguage["final"], 60))
+    println(currentLanguage["ratio"]?.format(
+        lessonName,
+        currentLanguage["final"],
+        60
+    ))
     var finalRatio: String? = readLine()
 
     if ((finalRatio !== null && finalRatio.isEmpty()) || finalRatio?.let { Helpers.isNumber(it) } == false) {
-        finalRatio = FINAL_RATIO.toString();
+        finalRatio = FINAL_RATIO.toString()
     }
+
+    val resultValue: Double = Helpers.getRealGrade(midtermValue.toInt(), midtermRatio?.toDouble()) + Helpers.getRealGrade(finalValue.toInt(), finalRatio?.toDouble())
+    println(currentLanguage["result"]?.format(
+        lessonName,
+        midtermValue,
+        finalValue,
+        resultValue.toString(),
+        if (resultValue <= 50) "Geçmez" else "Geçer"
+    ))
 }
